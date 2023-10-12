77045ac0298dcd355d4efd337d0befebddce22fa
Mercy <mercymwangi07@gmail.com>
2023-10-12 20:22:08 +0300

    Loading new updates

---
+++ models/expenseModel.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+
+const ExpenseSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true,
+        
+    },
+    amount: {
+        type:Number,
+        required:true,
+    },
+    type: {
+        type:String,
+        default:'expense',
+    },
+    date: {
+        type:Date,
+        required:true,
+    },
+    category: {
+        type:String,
+        required:true,
+
+    },
+    description: {
+        type: String,
+
+    }, 
+
+
+}, {timestamps:true})
+
+module.exports = mongoose.model('Expense', ExpenseSchema )
